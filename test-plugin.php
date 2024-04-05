dc50cacd779f26bf71973001684a5b657cb34bb3
Jaro <pixtweaks@protonmail.com>
2024-04-05 10:19:25 +0100

    Add files via upload

---
+++ test-plugin.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+* Plugin Name: Plugin made to test automatic updates
+* Plugin URI: https://wpspeeddoctor.com/
+* Description: Only for testing purposes
+* Version: 1.0.0
+* Author: Jaro Kurimsky
+* Author URI: https://wpspeeddoctor.com/
+* Text Domain: plugin-update-test
+* Update Date: 2024-04-05
+* Domain Path: /languages/
+* License: GPLv3
+* Requires at least: 5.0
+* Requires PHP: 7.0.0
+*/	
+
+// namespace test_plugin_namespace;
+
+defined( 'ABSPATH' ) || exit;
+
+const VERSION = '1.0.0';
+
